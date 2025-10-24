fffbf74ecaa6594b6e6b00304be07064f4dad86c
mindbreakfast <homegamego@gmail.com>
2025-10-24 10:20:53 +0500

    Update data.js

--- data.js
+++ data.js
@@ -2,55 +2,55 @@
 const CASINOS = [
     {
         "name": "Вавада", 
-        "desc": "Легендарное место, где легенды тоже проигрывают.", 
-        "promo": "VAVADA777", 
-        "categories": ["Топ"], 
+        "desc": "+300% к депозиту. +150 спинов", 
+        "promo": "JekaTvin", 
+        "categories": ["Топ", "Крипто"], 
         "img": "https://арчизнал.рф/img/vavada.jpg", 
         "url": "https://example.com/vavada" // Замените на реальную реф ссылку
     },
     {
         "name": "Стейк", 
-        "desc": "Сочный сайт, прожаренный на 100% неудачи.", 
-        "promo": "MEAT100", 
-        "categories": ["Топ"], 
+        "desc": "+300% к депозиту. +150 спинов", 
+        "promo": "JekaTvin", 
+        "categories": ["Топ", "Крипто"], 
         "img": "https://jekatvink.vercel.app/img/stake.png", 
         "url": "https://example.com/stake" // Замените на реальную реф ссылку
     },
     {
         "name": "Водка", 
-        "desc": "Пей, играй, проигрывай — в правильном порядке!", 
-        "promo": "SHOT2025", 
+        "desc": "+300% к депозиту. +150 спинов", 
+        "promo": "JekaTvin", 
         "categories": ["Новые"], 
         "img": "https://арчизнал.рф/img/vodka.jpg", 
         "url": "https://example.com/vodka" // Замените на реальную реф ссылку
     },
     {
         "name": "Буй", 
-        "desc": "Корабль фортуны, который уже утонул. Но весело!", 
-        "promo": "BOOM1", 
+        "desc": "+300% к депозиту. +150 спинов", 
+        "promo": "JekaTvin", 
         "categories": ["Топ"], 
         "img": "https://арчизнал.рф/img/booi.jpg", 
         "url": "https://example.com/booi" // Замените на реальную реф ссылку
     },
     {
         "name": "ПлейФортуна", 
-        "desc": "Фортуна играет, но не с тобой.", 
-        "promo": "FORT999", 
+        "desc": "+300% к депозиту. +150 спинов", 
+        "promo": "JekaTvin", 
         "categories": ["Новые"], 
         "img": "https://арчизнал.рф/img/playfortuna.jpg", 
         "url": "https://example.com/playfortuna" // Замените на реальную реф ссылку
     },
     {
         "name": "ДжойКазино", 
-        "desc": "Радость заканчивается, когда заходишь на сайт.", 
-        "promo": "JOY0", 
+        "desc": "+300% к депозиту. +150 спинов", 
+        "promo": "JekaTvin", 
         "categories": ["Топ"], 
         "img": "https://jekatvink.vercel.app/img/joycasino.jpg", 
         "url": "https://example.com/joycasino" // Замените на реальную реф ссылку
     },
     {
-        "name": "НЕ В МОЮ СМЕНУ", 
-        "desc": "Никакого скама! Только проверенные проекты!", 
+        "name": "СКАМ", 
+        "desc": "Никакого скама! Только проверенные мемы!", 
         "promo": "", 
         "categories": [], 
         "img": "https://jekatvink.vercel.app/img/scam.jpg", 
